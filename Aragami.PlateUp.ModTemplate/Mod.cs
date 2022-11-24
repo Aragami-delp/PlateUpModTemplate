5d1bdc985b91b9bb82ad49be6087efbc610cccc6
Aragami <tim.velling@hotmail.de>
2022-11-24 21:52:37 +0100

    Corrected path + prepared Mod.cs

--- Aragami.PlateUp.ModTemplate/Mod.cs
+++ Aragami.PlateUp.ModTemplate/Mod.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BepInEx;
+using KitchenLib;
+using System.Reflection;
 
 namespace Aragami.PlateUp.ModTemplate
 {
-    public class Mod
+    // guid must be unique and is recommended to be in reverse domain name notation
+    // mod name that is displayed to the player
+    // mod version must follow semver e.g. "1.2.3.4"
+    [BepInPlugin("example.guid", "Displayed mod name", "0.1.0")]
+    [BepInProcess("PlateUp.exe")]
+    public class Mod : BaseMod
     {
-        string x = "Hi";
+        // Game version this mod is designed for in semver
+        // e.g. ">=1.1.1" for all future
+        // e.g. ">=1.1.1 <=1.2.3" for all until
+        public Mod() : base(">=1.1.1", Assembly.GetExecutingAssembly()) { }
+
+        private void Awake()
+        {
+            Logger.LogInfo($"{Assembly.GetExecutingAssembly().GetName().Name} loaded!");
+        }
     }
 }
