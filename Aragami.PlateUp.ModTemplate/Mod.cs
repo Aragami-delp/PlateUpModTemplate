ee44699e854bcf5686a8cac260cfa6232dafa5a4
Aragami <tim.velling@hotmail.de>
2022-11-25 14:35:03 +0100

    Correct KitchenLib loading

--- Aragami.PlateUp.ModTemplate/Mod.cs
+++ Aragami.PlateUp.ModTemplate/Mod.cs
@@ -6,9 +6,10 @@ namespace Aragami.PlateUp.ModTemplate
 {
     // guid must be unique and is recommended to be in reverse domain name notation
     // mod name that is displayed to the player
-    // mod version must follow semver e.g. "1.2.3.4"
+    // mod version must follow semver e.g. "1.2.3"
     [BepInPlugin("example.guid", "Displayed mod name", "0.1.0")]
     [BepInProcess("PlateUp.exe")]
+    [BepInDependency("kitchenmods.kitchenlib")]
     public class Mod : BaseMod
     {
         // Game version this mod is designed for in semver
