5dcb29bc46352caed53a21d76874bddfa44375c1
Aragami <tim.velling@hotmail.de>
2022-12-22 12:39:17 +0100

    Prepaired for workshop template

--- Aragami.PlateUp.ModTemplate/Mod.cs
+++ Aragami.PlateUp.ModTemplate/Mod.cs
@@ -1,25 +1,63 @@
-﻿using BepInEx;
+﻿using Kitchen;
 using KitchenLib;
+using KitchenMods;
 using System.Reflection;
+using UnityEngine;
 
-namespace Aragami.PlateUp.ModTemplate
+// Namespace should have "Kitchen" in the beginning
+namespace KitchenModName
 {
-    // guid must be unique and is recommended to be in reverse domain name notation
-    // mod name that is displayed to the player and listed in the mods menu
-    // mod version must follow semver e.g. "1.2.3"
-    [BepInPlugin("example.guid", "Aragami.PlateUp.ModTemplate", "0.1.0")]
-    [BepInProcess("PlateUp.exe")]
-    [BepInDependency("kitchenmods.kitchenlib")]
     public class Mod : BaseMod
     {
+        // guid must be unique and is recommended to be in reverse domain name notation
+        // mod name that is displayed to the player and listed in the mods menu
+        // mod version must follow semver e.g. "1.2.3"
+        public const string MOD_GUID = "example.guid";
+        public const string MOD_NAME = "ModName";
+        public const string MOD_VERSION = "0.1.0";
+        public const string MOD_AUTHOR = "username";
+        public const string MOD_GAMEVERSION = ">=1.1.1";
         // Game version this mod is designed for in semver
-        // e.g. ">=1.1.1" for all future
-        // e.g. ">=1.1.1 <=1.2.3" for all until
-        public Mod() : base(">=1.1.1", Assembly.GetExecutingAssembly()) { }
+        // e.g. ">=1.1.1" current and all future
+        // e.g. ">=1.1.1 <=1.2.3" for all from/until
 
-        private void Awake()
+        public Mod() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly()) { }
+
+        protected override void Initialise()
         {
-            Logger.LogInfo("Aragami.PlateUp.ModTemplate loaded!");
+            base.Initialise();
+            // For log file output so the official plateup support staff can identify if/which a mod is being used
+            LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!"); 
+
+#if WorkshopMono
+            GameObject.DontDestroyOnLoad(new GameObject(MOD_NAME, typeof(ModMono)));
+#endif
         }
+
+        protected override void OnUpdate()
+        {
+            
+        }
+
+        #region Logging
+        // You can remove this, I just prefer a more standardized logging
+        public static void LogInfo(string _log) { Debug.Log($"{MOD_NAME} " + _log); }
+        public static void LogWarning(string _log) { Debug.LogWarning($"{MOD_NAME} " + _log); }
+        public static void LogError(string _log) { Debug.LogError($"{MOD_NAME} " + _log); }
+        public static void LogInfo(object _log) { LogInfo(_log.ToString()); }
+        public static void LogWarning(object _log) { LogWarning(_log.ToString()); }
+        public static void LogError(object _log) { LogError(_log.ToString()); }
+        #endregion
     }
+
+#if WorkshopMono
+    public class ModMono : MonoBehaviour
+    {
+        private void Awake() { }
+
+        private void Start() { }
+
+        private void Update() { }
+    }
+#endif
 }
--- Aragami.PlateUp.ModTemplate/Properties/AssemblyInfo.cs
+++ Aragami.PlateUp.ModTemplate/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Aragami.PlateUp.ModTemplate")]
+[assembly: AssemblyTitle("ModName")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Aragami.PlateUp.ModTemplate")]
+[assembly: AssemblyProduct("ModName")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
